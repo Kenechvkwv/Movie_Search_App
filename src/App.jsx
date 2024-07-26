5b113e2acf1937c7f0965370c19099614a4fb653
Kenechukwu Ubesie <ubesiekene@gmail.com>
2024-07-26 18:15:39 +0100

    code debug

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,33 @@
-import { useContext, useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import Home from "./Components/Home";
 import MoviePage from "./Components/MoviePage";
-import { MovieContext } from "./context/MovieContext";
 
 const apiKey = "b5d5c542cf406e00c3a5afa8a711e711";
 const url = `https://api.themoviedb.org/3/movie/157336/videos?api_key=${apiKey}`;
-let mArray = [];
+
 function App() {
   const [movies, setMovies] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
-
-  const [count, setCount] = useState(0);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await fetch(url);
         const data = await response.json();
-        // console.log(data);
-        mArray = data.results;
-        setMovies(mArray);
-        setLoading(false);
+        setMovies(data.results);
       } catch (error) {
-        setError(error);
+        setError(error.message);
+      } finally {
         setLoading(false);
       }
     };
     fetchData();
-    // console.log(fetchData())
   }, []);
-  console.log(movies);
+
+  if (loading) return <p>Loading...please wait</p>;
+  if (error) return <p>Error: {error}</p>;
+
   return (
     <>
       <Home />
